71bdd914ded5588ffbe44f5cdd24390810d040a2
vaibhavi.ladva <ladvaibhavi26@gmail.com>
2024-02-20 17:48:17 -0800

    commit at 20 feb

--- src/main/java/com/example/jdbidemo/Config/SecurityConfig.java
+++ src/main/java/com/example/jdbidemo/Config/SecurityConfig.java
@@ -2,13 +2,22 @@ package com.example.jdbidemo.Config;
 
 import com.example.jdbidemo.security.JwtAuthenticationEntryPoint;
 import com.example.jdbidemo.security.JwtAuthenticationFilter;
+import com.example.jdbidemo.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 
 @Configuration
 public class SecurityConfig {
@@ -18,6 +27,10 @@ public class SecurityConfig {
     private JwtAuthenticationEntryPoint point;
     @Autowired
     private JwtAuthenticationFilter filter;
+    @Autowired
+    private UserDetailsService usersService;
+@Autowired
+private PasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -33,5 +46,12 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(usersService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return daoAuthenticationProvider;
+    }
 
 }
